1c3258001c64baa7ba882c0b74fc3459859fd2b4
Raphael Nobre <nobre.roots@gmail.com>
2022-11-28 12:53:33 -0300

    adição tecnologias

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import header from "./header"
-
-const app = () => {
-    return (
-        <div>
-            <header/>
-        </div>
-    )
-    
+// Link dos projetos
+/*document.getElementById("rocketPay").onclick = function () {
+    window.open("https://rocketpay-ignitelab.vercel.app");
 }
+
+document.getElementById("google").onclick = function () {
+    window.open("https://raphaelnb.github.io/google/");
+}
+
+document.getElementById("orcestra").onclick = function () {
+    window.open("https://mariaabritta.github.io/SiteProject/#orcestra");
+}*/
